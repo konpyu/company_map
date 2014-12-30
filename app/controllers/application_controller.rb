9d45f078014233c045179722f92e4ff42dbb9abe
kon.yuichi <konpyu@gmail.com>
2014-12-30 17:50:26 +0900

    add error-handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    respond_to do |format|
+      format.json { render json: { error: "resource not found" } , status: 404 }
+    end
+  end
+
 end
